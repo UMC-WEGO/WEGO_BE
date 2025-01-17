6636d317366c595e634a38b6d10995a691691f41
Jaen-923 <113357550+Jaen-923@users.noreply.github.com>
2025-01-17 17:18:15 +0900

    docs : swagger 문서 수정

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -7,3 +7,11 @@ export const authRouter = express.Router();
 
 // 회원가입 API
 authRouter.post('/signUp', signUpController);
+authRouter.post('/email-auth/send', );  
+authRouter.post('/email-auth/verify', );  
+authRouter.post('/login', );
+authRouter.post('/refresh', );  
+authRouter.post('/delete', );   
+authRouter.post('/logout', );
+authRouter.post('/nickname-check', );
+authRouter.post('/email-check', );
