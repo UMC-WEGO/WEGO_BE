b785d8270deb7434de67542a5104268f9af46c66
Jaen-923 <113357550+Jaen-923@users.noreply.github.com>
2025-01-29 21:18:24 +0900

    refactor : Cors 설정 변경

--- src/index.js
+++ src/index.js
@@ -16,7 +16,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());                            // cors 방식 허용
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Authorization"], 
+  })
+);                          
 app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
